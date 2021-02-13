d2a5daba71d0c3a7cf158f3c5439233bc4b4f8f9
Creator Piyush <piyushanand.1221@gmail.com>
2021-02-13 20:52:25 +0530

    signin page sequelize

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
+const db = require("./models");
+
+const { User } = require("./models");
+
+app.use(express.json());
+app.use(cors());
 
 const port = process.env.PORT || 5555;
 
@@ -7,6 +15,24 @@ app.get("/", (req, res) => {
   res.send(`Hello World form ${port}`);
 });
 
-app.listen(port, () => {
-  console.log(`Server started at http://localhost:${port}`);
+app.post("/signin", (req, res) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  const googleId = req.body.googleId;
+
+  User.create({
+    name: name,
+    email: email,
+    googleId: googleId,
+  }).catch((err) => {
+    console.log(err);
+  });
+
+  res.send("inserted into db");
+});
+
+db.sequelize.sync().then((req) => {
+  app.listen(port, () => {
+    console.log(`Server started at http://localhost:${port}`);
+  });
 });
---
+++ models/Users.js
@@ -0,0 +1,29 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define("User", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+
+    googleId: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+  });
+
+  return User;
+};
